443ddaecdb248d48490772350f508136beaf9eb1
Erik <eculberson12@gmail.com>
2016-04-23 03:15:40 -0400

    Added temp logging and create user

--- json_to_database.go
+++ json_to_database.go
@@ -15,6 +15,7 @@ type Configuration struct {
 }
 
 type database struct {
+	Default    string
 	Name       string
 	Connection Connection
 	Users      Users
@@ -49,12 +50,20 @@ type Column struct {
 }
 
 func GetDatabaseConnection(database_struct database) *sql.DB {
-	database_name := strings.Split(database_struct.Name, "{")
+	fmt.Println("Parsing Database connection information")
+	//database_name := strings.Split(database_struct.Name, "{")
 	database_user := strings.Split(database_struct.Users.DefaultUser.Username, "{")
 	database_password := strings.Split(database_struct.Users.DefaultUser.Password, "{")
 
+	fmt.Println("Connecting to default database:",
+		"\nDatabase Name=", database_struct.Default,
+		"\nDatabase Host=", database_struct.Connection.Host,
+		"\nDatabase Port=", database_struct.Connection.Port,
+		"\nDatabase User=", database_user[0],
+		"\nDatabase Password=", database_password[0])
+
 	db := postgresql.ConnectToDatabase(
-		database_name[0],
+		database_struct.Default,
 		database_struct.Connection.Host,
 		database_struct.Connection.Port,
 		database_user[0],
@@ -64,15 +73,35 @@ func GetDatabaseConnection(database_struct database) *sql.DB {
 }
 
 func JsonToDatabse() {
+	fmt.Println("Loading Json Configuration")
 	configuration := testJson()
 
+	fmt.Println("Traversing database structure")
 	for _, database_struct := range configuration.Database {
+		fmt.Println("Getting database connection")
 		db := GetDatabaseConnection(database_struct)
 
+		fmt.Println("Checking if Roles exist")
+		for _, user := range database_struct.Users.User {
+			user_name := strings.Split(user.Username, "{")
+			user_roles := strings.Replace(user.Role, " ", "", -1)
+			user_roles = strings.Replace(user_roles, ",", " ", -1)
+
+			fmt.Println("Checking if user exitst:", user_name[0])
+			user_exist, _ := postgresql.CheckIfRoleExists(db, user_name[0])
+			//TODO: Check if roles match
+			if !user_exist {
+				fmt.Println("User does not exits, Creating User")
+				postgresql.CreateUser(db, user_name[0], user_roles)
+			}
+		}
+		return
+
+		fmt.Println("Checking if Database:", database_struct.Name, "Exists")
 		exist, _ := postgresql.CheckIfDatabaseExists(db, database_struct.Name)
 
 		if !exist {
-			fmt.Println(exist)
+			fmt.Println("Database does not exits, Creating database")
 			postgresql.CreateDatabase(db, database_struct.Name)
 		}
 	}
@@ -109,27 +138,31 @@ func JsonToDatabse() {
 }
 
 func main() {
+	fmt.Println("Running Json to Database")
 	JsonToDatabse()
 }
 
 func testJson() Configuration {
 	path, _ := os.Getwd()
-
-	//dat, _ := ioutil.ReadFile(path + "/db.conf.json")
-	//fmt.Print(string(dat))
+	fmt.Println("Loading test Json Configuration at path: ", path)
 
 	config_file, err := os.Open(path + "/db.conf.json")
 	if err != nil {
+		fmt.Println("Test Json Configuration failed to loaded successfully")
 		fmt.Println(err)
 	}
+	fmt.Println("Test Json Configuration was loaded successfully")
 
 	var configuration Configuration
 
+	fmt.Println("Decoding json")
 	decoder := json.NewDecoder(config_file)
 	err = decoder.Decode(&configuration)
 	if err != nil {
+		fmt.Println("Failed to Decoding json")
 		fmt.Println(err)
 	}
+	fmt.Println("Decoding json was sucessful")
 	//fmt.Println(configuration)
 	return configuration
 }
--- postgresql/createdb.go
+++ postgresql/createdb.go
@@ -9,28 +9,66 @@ import (
 
 func ConnectToDatabase(dbname string, host string, port int, username string, password string) *sql.DB {
 	db_url := fmt.Sprintf("postgres://%s:%s@%s/%s", username, password, host, dbname)
-	fmt.Println(db_url)
+	fmt.Println("Connecting to database:", db_url)
 	db, err := sql.Open("postgres", db_url)
 	if err != nil {
+		fmt.Println("Failed to connecting to database")
 		fmt.Println(err)
+		return nil
 	}
+	fmt.Println("Successfuly connected to database")
 	return db
 }
 
 func CheckIfDatabaseExists(db *sql.DB, database_name string) (bool, error) {
 	statement := fmt.Sprintf("select count(*) from pg_database where datname='%s'", database_name)
-	fmt.Println(statement)
-	_, count, _ := postgresql_access.QueryDatabase(db, statement)
-	if count == 1 {
+	fmt.Println("Checking if database exists with the statement:", statement)
+	count, _, _ := postgresql_access.QueryDatabase(db, statement)
+	fmt.Println("database statement count returned:", count)
+	if count[0][0] == 1 {
+		fmt.Println("Database exists")
 		return true, nil
 	}
+	fmt.Println("Database does not exists")
 	return false, nil
 }
 
 func CreateDatabase(db *sql.DB, database_name string) {
 	statement := fmt.Sprintf("CREATE DATABASE %s", database_name)
-	err := postgresql_access.CreateDatabase(db, statement)
-	fmt.Println(err)
+	fmt.Println("Creating database:", statement)
+	//err := postgresql_access.CreateDatabase(db, statement)
+	_, err := db.Exec(statement)
+	if err != nil {
+		fmt.Println("Failed to creating database")
+		fmt.Println(err)
+		return
+	}
+	fmt.Println("Successfully created database")
+}
+
+func CheckIfRoleExists(db *sql.DB, user_name string) (bool, error) {
+	statement := fmt.Sprintf("select count(*) from pg_user where usename='%s'", user_name)
+	fmt.Println("Checking if user exists with the statement:", statement)
+	count, _, _ := postgresql_access.QueryDatabase(db, statement)
+	fmt.Println("database statement count returned:", count)
+	if count[0][0] == 1 {
+		fmt.Println("User exists")
+		return true, nil
+	}
+	fmt.Println("User does not exists")
+	return false, nil
+}
+
+func CreateUser(db *sql.DB, user_name, user_roles string) {
+	statement := fmt.Sprintf("CREATE ROLE %s WITH %s", user_name, user_roles)
+	fmt.Println("Creating database:", statement)
+	_, err := db.Exec(statement)
+	if err != nil {
+		fmt.Println("Failed to creating user")
+		fmt.Println(err)
+		return
+	}
+	fmt.Println("Successfully created user")
 }
 
 /*
